126692d82534608eee15cb7b386a34258b99a5ea
dongmy54 <615760433@qq.com>
2017-03-26 15:54:37 +0800

    install capistrano-passenger edit Capfile and config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,3 +44,5 @@ set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/
 # append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
+
+set :passenger_restart_with_touch, true
